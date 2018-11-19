9576848e934f917d78164b1b0229a17c39266c47
Karen Ho <kho@financeit.io>
2018-11-19 13:17:54 -0500

    Add address column to locations; add geocoder options to Location model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_170906) do
+ActiveRecord::Schema.define(version: 2018_11_19_175507) do
 
   create_table "locations", force: :cascade do |t|
     t.integer "neighbourhood_id"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_11_19_170906) do
     t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
     t.index ["neighbourhood_id"], name: "index_locations_on_neighbourhood_id"
   end
 
