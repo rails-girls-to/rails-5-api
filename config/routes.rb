ee9e05eeeb7d4860adcc6607b11639474fdfd36b
Karen Ho <kho@financeit.io>
2018-11-19 13:27:27 -0500

    Remove unnecessary location controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :locations
   resources :neighbourhoods
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
